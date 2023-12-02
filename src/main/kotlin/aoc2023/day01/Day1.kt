09b9c30aeb9db673ed5a0a4b928b2c25074ff60b
Jonathan Nye <jonba2@gmail.com>
2023-12-02 11:01:33 -0500

    Day 1 part 2

--- src/main/kotlin/aoc2023/day01/Day1.kt
+++ src/main/kotlin/aoc2023/day01/Day1.kt
@@ -1,21 +1,20 @@
 package aoc2023.day01
 
+import util.assertEqual
 import util.readResourceLines
-import java.io.File
 
 fun main() {
     val sampleInput = readResourceLines("/2023/day01sample.txt")
     val sampleSolution = part1(sampleInput)
-    assert(sampleSolution == 77)
+    assertEqual(sampleSolution, 142)
 
     val problemInput = readResourceLines("/2023/day01input.txt")
-//    println(part1(problemInput))
+    println(part1(problemInput))
 
     val sampleInput2 = readResourceLines("/2023/day01sample2.txt")
-//    val sampleSolution2 = part2(sampleInput2)
-//    assert(sampleSolution2 == 281)
-//    println(part2(problemInput))
-    part2(problemInput)
+    val sampleSolution2 = part2(sampleInput2)
+    assertEqual(sampleSolution2, 281)
+    println(part2(problemInput))
 }
 
 private fun part1(lines: List<String>): Int {
@@ -26,32 +25,49 @@ private fun part1(lines: List<String>): Int {
 }
 
 private fun part2(lines: List<String>): Int {
-    val digitMappings = listOf(
-        "zero" to "0",
-        "one" to "1",
-        "two" to "2",
-        "three" to "3",
-        "four" to "4",
-        "five" to "5",
-        "six" to "6",
-        "seven" to "7",
-        "eight" to "8",
-        "nine" to "9",
-    )
-    fun String.replaceAllInstancesOf(mappings: List<Pair<String, String>>): String {
-        var working = this
-        for (mapping in mappings) {
-            working = working.replace(mapping.first, mapping.second)
-        }
-        return working
-    }
     return lines
-        .map { it.replaceAllInstancesOf(digitMappings) }
-        .onEach {
-            println(it)
-        }
         .sumOf { line ->
-            val digits = line.filter { it.isDigit() }
-            (digits.first().toString() + digits.last()).toInt()
+            val foundDigits = line.findDigits()
+            (foundDigits.first().toString() + foundDigits.last()).toInt()
         }
+}
+
+fun String.findDigits(): List<Int> {
+    val foundAsDigits = this.mapIndexed { idx, char -> idx to char }
+        .filter { (_, char) -> char.isDigit() }
+    val foundAsWords = digitMappings.keys.flatMap { digitString ->
+        this
+            .indicesOf(digitString)
+            .map { idx -> idx to (digitMappings[digitString] ?: throw IllegalStateException("Couldn't map $digitString")) }
+    }
+    return (foundAsDigits + foundAsWords)
+        .sortedBy { it.first }
+        .map { it.second.digitToInt() }
+}
+
+private val digitMappings = mapOf(
+    "zero" to '0',
+    "one" to '1',
+    "two" to '2',
+    "three" to '3',
+    "four" to '4',
+    "five" to '5',
+    "six" to '6',
+    "seven" to '7',
+    "eight" to '8',
+    "nine" to '9',
+)
+private fun String.indicesOf(substring: String): List<Int> {
+    var startIdx = 0
+    val results = mutableListOf<Int>()
+    while (true) {
+        val foundIdx = this.indexOf(substring, startIdx)
+        if (foundIdx == -1) {
+            break
+        } else {
+            results.add(foundIdx)
+        }
+        startIdx = foundIdx + 1
+    }
+    return results
 }
--- src/main/kotlin/util/Utils.kt
+++ src/main/kotlin/util/Utils.kt
@@ -3,3 +3,7 @@ package util
 fun readResourceLines(path: String): List<String> {
     return object {}.javaClass.getResourceAsStream(path)?.bufferedReader()?.readLines().orEmpty()
 }
+
+fun <T> assertEqual(actual: T, expected: T) {
+    if (actual != expected) throw IllegalStateException("Expected $expected, got $actual")
+}
